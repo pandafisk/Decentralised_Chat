86715e37dd458ed72fea44eb76c04280f27089c4
MikPos <38103474+MikPos@users.noreply.github.com>
2021-05-17 11:00:59 +0200

    test updated with other functions

--- test.erl
+++ test.erl
@@ -8,23 +8,46 @@ init() ->
     mnesia:start(),
     mnesia:create_schema([]).
 
+%% ------------------------------------------
+%% FUNCTIONS FOR DEALING WITH COMMUNICATION
+%% ------------------------------------------
 
+%% A function for creating a new group.
 new_group(Name) ->
     mnesia:create_table(Name, [
         {attributes, record_info(fields, msg)}, 
         {record_name, msg},
         {type, ordered_set}]).
 
-put(Table, Name, Message) ->
+
+%% A function for joining an existing group.
+join(Group, User) ->
+    %message = io_lib:format("~p joined the  group!", [User),
+    sendMsg(Group, User, User).
+
+
+%% A functions that sends a message to the group.
+sendMsg(Group, User, Message) ->
     Insert = fun() -> 
-        mnesia:write(Table, #msg{
+        mnesia:write(Group, #msg{
             date = erlang:timestamp(),
-            name = Name,
+            name = User,
             message = Message}, write)
     end,
-   mnesia:transaction(Insert).
+    {_, _, _, Users} = fun() -> 
+        mnesia:match_object({Group, '_', '_', '_'})
+    end,
+    newMessage = io_lib:format("(~p - ~p): ~p", [Group, User, Message]),
+    send(Users, newMessage),
+    mnesia:transaction(Insert).
 
 
+
+%% ------------------------------------------
+%% FUNCTIONS FOR DATABASE INTERACTION
+%% ------------------------------------------
+
+%% A function for displaying message history of a group.
 msg_history(Table_name)->
     Iterator =  fun(Rec,_)->
                     {_, Time, Msg, Name} = Rec,
@@ -36,4 +59,53 @@ msg_history(Table_name)->
         false -> 
             Exec = fun({Fun,Tab}) -> mnesia:foldl(Fun, [],Tab) end,
             mnesia:activity(transaction,Exec,[{Iterator,Table_name}],mnesia_frag)
+    end.
+
+
+%% A function for listing users in a group.
+users(Group) ->
+    {_, _, _, Users} = fun() -> 
+        mnesia:match_object({Group, '_', '_', '_'})
+    end,
+    Query = uniques(Users),
+    mnesia:transaction(Query).
+
+
+%% A function for finding a User, based on Username.
+findUser(User) ->
+    Query = fun() -> 
+        mnesia:match_object({'_', '_', '_', User})
+    end,
+    mnesia:transaction(Query).
+
+
+%% A function for finding a Group, based on Group name.
+findGroup(Group) ->
+    Query = fun() -> 
+        mnesia:match_object({Group, '_', '_', '_'})
+    end,
+    mnesia:transaction(Query).
+
+
+
+
+%% ------------------------------------------
+%% LOCAL FUNCTIONS
+%% ------------------------------------------
+
+%% Send a message to all users
+send([PID | Rest], msg) ->
+    PID ! msg,
+    send(Rest, msg).
+
+%% Find unique elements in a list
+uniques(L) ->
+    uniques(L, [], []).
+
+uniques([], _, Acc) ->
+    lists:reverse(Acc);
+uniques([X | Rest], Seen, Acc) ->
+    case lists:member(X, Seen) of
+        true -> uniques(Rest, Seen, Acc);
+        false -> uniques(Rest, [X | Seen], [X | Acc])
     end.
