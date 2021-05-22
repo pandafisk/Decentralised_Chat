7080b33ed8ca7fa865c3d5cca2cd166444fbb1f8
MikPos <38103474+MikPos@users.noreply.github.com>
2021-05-22 12:43:23 +0200

    Add files via upload

---
+++ Final/chat_client.erl
@@ -0,0 +1,102 @@
+-module(chat_client).
+
+%% Internal
+-export([createGroup/1, sendMessage/2, viewHistory/1, listUsers/1, findUser/2, findGroup/1]).
+%% Remote
+-export([remote_createGroup/2, remote_sendMessage/4, remote_viewHistory/2, remote_listUsers/2, remote_findUser/3, remote_findGroup/2]).
+%% Helpers
+-export([get_server/0, connect_client/1]).
+
+
+%% ======================================================
+%%               Internal Calls
+%% ======================================================
+%% These function can only be called internally from a server-node
+
+%% Creates a new group with the name group.
+createGroup(Group) ->
+    chat_server:newGroup(Group).
+
+%% Sends a message to all users in the group with name Group.
+sendMessage(Group, Message) ->
+    chat_server:sendMessage(Group, node(), Message),
+    GroupUsers = chat_server:users(Group),
+    showHistoryToAll(GroupUsers, Group).
+
+%% Checks if a user exists, by userame.
+findUser(Group, User) ->
+    chat_server:findUser(Group, User).
+
+%% Checks if a group exists, by group name.
+findGroup(Group) ->
+    chat_server:findGroup(Group).
+
+%% List all Users that is connected to the group
+listUsers(Group) ->
+    chat_server:users(Group).
+
+%% Lists the history of the chats in the group
+viewHistory(Table) ->
+    chat_server:history(Table).
+
+%% ======================================================
+%%              Remote Calls
+%% ======================================================
+%% These functions are intended to be called from a remote client-node
+%% Can also be called from a server-node
+
+%% Remote version of createGroup.
+remote_createGroup(Server, Group) ->
+    rpc:call(Server, chat_client, createGroup, [Group]).
+
+%% remote version of sendMessage.
+remote_sendMessage(Server, Group, User, Message) ->
+    rpc:call(Server, chat_client, sendMessage, [Group, User, Message]).
+
+%% Remote version of findUser.
+remote_findUser(Server, Group, User) ->
+    rpc:call(Server, chat_client, findUser, [Group, User]).
+
+%% Remote version of findGroup.
+remote_findGroup(Server, Group) ->
+    rpc:call(Server, chat_client, findGroup, [Group]).
+
+%% Remote version of listUsers.
+remote_listUsers(Server, Group) ->
+    rpc:call(Server, chat_client, listUsers, [Group]).
+
+%% Remote version of viewHistory.
+remote_viewHistory(Server, Group) ->
+    io:format("---Server:~p~n",[Server]),
+    rpc:call(Server, chat_client, viewHistory, [Group]).
+
+
+%% ======================================================
+%%              Helpers
+%% ======================================================
+
+% Helper function for showing the chat history in all other users terminals, within a given group.
+% It is used for sendMessage.
+showHistoryToAll([], _) ->
+    io:format("---END OF CHAT HISTORY--~n");
+
+showHistoryToAll([User|GroupUsers], Group) ->
+    io:format("---USER:~p~n",[User]),
+    io:format("---Group Name:~p~n",[Group]),
+    rpc:call(User, chat_client, viewHistory, [Group]),
+    showHistoryToAll(GroupUsers, Group).
+
+
+%% Helper functions to connect a client to the server
+
+%% Returns a running server to the client
+connect_client(Host) ->
+    rpc:call(Host, chat_client, get_server, []).
+
+%% Selects a random node from one of the servers, to be used as dedicated server for a client
+%% Works as a load-balancer
+get_server() ->
+    Servers = mnesia:table_info(test, where_to_write),
+    lists:nth(rand:uniform(length(Servers)), Servers).
+
+
---
+++ Final/chat_server.erl
@@ -0,0 +1,99 @@
+-module(chat_server).
+
+-behaviour(gen_server).
+
+-export([start_link/0, start/1]).
+%%Gen_server
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%%Public
+-export([newGroup/1, joinGroup/2, sendMessage/3, findUser/2, findGroup/1, users/1, history/1]).
+
+
+-record(state, {}).
+
+%% ============================================
+%%          Client Call
+%% ============================================
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+start(Name) ->
+  gen_server:start({local, Name}, ?MODULE, [], []).
+
+newGroup(Group) ->
+    gen_server:call(?MODULE, {new_group, Group}).
+
+joinGroup(Group, User) ->
+    gen_server:call(?MODULE, {join, Group, User}).
+
+sendMessage(Group, User, Message) ->
+    gen_server:call(?MODULE, {sendMessage, Group, User, Message}).
+
+findUser(Group, User) ->
+    gen_server:call(?MODULE, {findUser, Group, User}).
+
+findGroup(Group) ->
+    gen_server:call(?MODULE, {findGroup, Group}).
+
+users(Group) ->
+    gen_server:call(?MODULE, {users, Group}).
+
+history(Table) ->
+    gen_server:call(?MODULE, {history, Table}).
+
+%% ============================================
+%%          Call Back Functions
+%% ============================================
+
+init(_Args) ->
+    process_flag(trap_exit, true),
+    io:format("~p (~p) starting...~n", [{local, ?MODULE}, self()]),
+    test:init(),
+    {ok, #state{}}.
+
+handle_call({new_group, Group}, _From, State) ->
+    test:new_group(Group),
+    io:format("~p (~p) The group ~p has been created.~n",[?MODULE, self(), Group]),
+    {reply, ok, State};
+
+handle_call({sendMessage, Group, User, Message}, _From, State) ->
+    test:sendMsg(Group, User, Message),
+    io:format("~p (~p) You have sent your message to all users in ~p.~n",[?MODULE, self(), Group]),
+    {reply, ok, State};
+
+handle_call({findUser, Group, User}, _From, State) ->
+    test:findUser(Group, User),
+    io:format("~p (~p) User: ~p Exists.~n",[?MODULE, self(), User]),
+    {reply, ok, State};
+
+handle_call({findGroup, Group}, _From, State) ->
+    test:findGroup(Group),
+    io:format("~p (~p) Group: ~p Exists.~n",[?MODULE, self(), Group]),
+    {reply, ok, State};
+
+handle_call({users, Group}, _From, State) ->
+    Users = test:findUniques(Group),
+    io:format("~p (~p) Users: ~p~n",[?MODULE, self(), Users]),
+    {reply, Users, State};
+
+handle_call({history, Table}, _From, State) ->
+    Chat = test:msg_history(Table),
+    io:format("~p (~p) Messages: ~p~n",[?MODULE, self(), Chat]),
+    {reply, Chat, State};
+
+
+handle_call( _Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
